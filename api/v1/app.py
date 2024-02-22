573c839b2625bcade5612628685c12cb44b49645
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-22 19:42:32 +0100

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -11,6 +11,11 @@ app.register_blueprint(app_views)
 def close(exception):
     """ closes connection/sql session on teardown """
     storage.close()
+
+@app.errorhandler(404)
+def error_404(arror):
+    error_dic = {"error": "Not found"}
+    return jsonify(error_dic)
 
 if __name__ == "__main__":
     """this is our main flask applicataion"""
