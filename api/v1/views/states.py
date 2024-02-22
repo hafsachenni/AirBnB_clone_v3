1a8790e3225c72da5ded1a6bcfc09bccd2ba85ee
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-22 23:05:50 +0100

    task 6 v2

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from models.state import State
-from flask import Flask, jsonify,Blueprint , request, abort
+from flask import Flask, jsonify,Blueprint , request, abort, make_response
 from api.v1.views import app_views
 from models import storage
 
@@ -12,10 +12,21 @@ def gets_all_states():
     dict_states= [state.to_dict() for state in all_states]
     return jsonify(dict_states)
 
-@app_views.route("/states/<state_id>", methods=["GET"])
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def states(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort (404)
         #return error_404()
     return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def state_dele(state_id):
+    state = storage.get(state, state_id)
+    if not state:
+        abort (404)
+        #return error_404()
+
+    storage.delete(state)
+    storage.save()
+    return make_response(jsonify({}), 200)
