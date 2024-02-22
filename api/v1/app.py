4a03b686ff4161f049689d0e6ac60ade6e3f56f2
hafsa chenni <hafsachenni77@gmail.com>
2024-02-22 18:16:06 +0100

    flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
 
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close(exception):
+    """ closes connection/sql session on teardown """
+    storage.close()
+
+if __name__ == "__main__":
+    """this is our main flask applicataion"""
+    """and we are retrieving the value of environment vars"""
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
