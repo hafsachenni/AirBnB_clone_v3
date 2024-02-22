c0f364eb285c35aab69a12c812043d1705ccaa24
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-22 22:12:11 +0100

    task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,12 +2,20 @@
 from models.state import State
 from flask import Flask, jsonify,Blueprint , request, abort
 from api.v1.views import app_views
-from api.v1.app import error_404
+from models import storage
 
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def gets_all_states():
+    """gets info about all states from storage"""
+    all_states = storage.all(State) .values()
+    dict_states= [state.to_dict() for state in all_states]
+    return jsonify(dict_states)
 
 @app_views.route("/states/<state_id>", methods=["GET"])
 def states(state_id):
     state = storage.get(State, state_id)
     if not state:
-        # abort (404)
-        return error_404()
+        abort (404)
+        #return error_404()
+    return jsonify(state.to_dict())
