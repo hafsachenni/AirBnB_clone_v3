9a9e07cad3b5623c3c9a73c9f157c63fdf63c809
hafsa chenni <hafsachenni77@gmail.com>
2024-02-22 19:18:35 +0100

    init file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route("/status", methods=['GET'])
 def hbnb_stats():
     """returns json status of our flask app"""
     return jsonify({"status": "OK"})
