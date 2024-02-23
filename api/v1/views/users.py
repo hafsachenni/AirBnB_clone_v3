42cee206bb8f7b2f1cd2e174eda522a9fa4a1599
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-23 19:13:20 +0100

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -13,6 +13,7 @@ def get_all_users():
     user = [user.to_dict() for user in users]
     return jsonify(user)
 
+
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def user_id(user_id):
     """retriveving a user from storage based on its provided user_id"""
@@ -20,6 +21,7 @@ def user_id(user_id):
     if not user:
         abort(404)
     return jsonify(user.to_dict())
+
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
@@ -31,18 +33,20 @@ def delete_user(user_id):
     storage.save()
     return (jsonify({}), 200)
 
+
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def post_user():
     """posting a new user"""
-    if not request.get_json():
+    if not request.get_json(silent=True):
         abort(400, 'Not a JSON')
-    if 'email' not in request.get_json():
+    if 'email' not in request.get_json(silent=True):
         abort(400, 'Missing email')
-    if 'password' not in request.get_json():
+    if 'password' not in request.get_json(silent=True):
         abort(400, 'Missing password')
-    new_user = User(**request.get_json())
+    new_user = User(**request.get_json(silent=True))
     storage.save()
     return (jsonify(new_user.to_dict()), 201)
+
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
@@ -50,10 +54,10 @@ def put_user(user_id):
     user = storage.get(User, user_id)
     if not user:
         abort(404)
-    if not request.get_json():
+    if not request.get_json(silent=True):
         abort(400, 'Not a JSON')
     """iterating through each key/value in the request body"""
-    for key, value in request.get_json().items():
+    for key, value in request.get_json().items(silent=True):
         if key not in ['id', 'email', 'created_at', 'updated_at']:
             """setting value of attr"""
             setattr(user, key, value)
