5af62992e5e210aff9144250c0cbe9d3bf2b08c7
hafsa chenni <hafsachenni77@gmail.com>
2024-02-23 17:23:06 +0100

    merging

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """ new view for State objects that handles all default RESTFul API actions """
 from models.state import State
 from flask import Flask, jsonify, Blueprint, request, abort, make_response
@@ -11,29 +10,29 @@ from models import storage
 def gets_all_states():
     """gets info about all states from storage"""
     all_states = storage.all(State) .values()
-    dict_states= [state.to_dict() for state in all_states]
+    dict_states = [state.to_dict() for state in all_states]
     return jsonify(dict_states)
+
 
 @app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def states(state_id):
     """ Retrieves the list of all State objects: GET /api/v1/states
     Retrieves a State object: GET /api/v1/states/<state_id>
     If the state_id is not linked to any State object, raise a 404 error """
-
     state = storage.get(State, state_id)
     if not state:
-        abort (404)
-        #return error_404()
+        abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
+
+@app_views.route("/states/<state_id>", methods=["DELETE"],
+                 strict_slashes=False)
 def states_delete(state_id):
     """ delete an object in states  """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-        #return error_404()
-    storage.delete(state)    
+    storage.delete(state)
     storage.save()
     return make_response(jsonify({}), 200)
 
@@ -45,11 +44,12 @@ def post():
     if not request.get_json():
         return (jsonify({'error: Not a JSON'}), 400)
     if 'name' not in request.get_json():
-        return(jsonify({'error: Missing name'}), 400)
+        return (jsonify({'error: Missing name'}), 400)
     """creation of new state, we added a state in the request body"""
     state = State(**request.get_json())
     state.save()
     return (jsonify(state.to_dict()), 201)
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
