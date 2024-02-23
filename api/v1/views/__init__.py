9b5713597ae5457fba6ca6adfa307e13652483c3
hafsa chenni <hafsachenni77@gmail.com>
2024-02-23 17:12:22 +0100

    docs

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+"""craetion of flask blueprint"""
 from flask import Blueprint
 
-
-"""creation of a flask blueprint named app_views"""
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
@@ -10,3 +9,4 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+from api.v1.views.places import *
