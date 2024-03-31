91152534f7394492fdf01004bdbbc3f0810c318c
hafsa chenni <hafsachenni77@gmail.com>
2024-03-31 17:09:25 +0200

    instance of flask

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprints
+from flask import Blueprint
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" an endpoint that retrieves the number of each objects by type """
+"""endpoint that retrieves the number of each objects by type """
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
@@ -13,7 +13,7 @@ def hbnb_stats():
 
 @app_views.route("/stats", strict_slashes=False)
 def stats():
-    """ retrieves the number of each objects by type  """
+    """ this retrieves the number of each objects by type  """
     stats_dic = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
