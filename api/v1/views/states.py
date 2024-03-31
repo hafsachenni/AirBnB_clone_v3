2234167963fdcf059bc6ea0d724e48ca2f5cf06a
hafsa chenni <hafsachenni77@gmail.com>
2024-03-31 17:41:41 +0200

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -34,7 +34,7 @@ def states_delete(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
